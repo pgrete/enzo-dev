f8d58543c592b46873f00605e75eddae286ba86d
Wolfram Schmidt <wolfram.schmidt@uni-hamburg.de>
2017-04-20 14:06:40 +0200

    divB bug fix in AllocateGrids, configuration file for supermuc (Intel).

--- src/enzo/Grid_AllocateGrids.C
+++ src/enzo/Grid_AllocateGrids.C
@@ -63,4 +63,28 @@ void grid::AllocateGrids()
     
   } // if(UseMHDCT)
 
+  int activesize = 1;
+  for (int dim = 0; dim < GridRank; dim++) {
+    activesize *= (GridDimension[dim] - 2 * NumberOfGhostZones);
+  }
+
+  if (HydroMethod == MHD_RK) {
+
+    if (divB == NULL) {
+      divB = new float[activesize];
+    }
+
+    for (int dim = 0; dim < 3; dim++) {
+      if (gradPhi[dim] == NULL) {
+        gradPhi[dim] = new float[activesize];
+      }
+    }
+
+    for (int dim = GridRank; dim < 3; dim++) {
+      for (int n = 0; n < activesize; n++) {
+        gradPhi[dim][n] = 0.0;
+        divB[n] = 0.0;
+      }
+    }
+  }
 }
